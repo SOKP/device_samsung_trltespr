fe388c919d6a6285aae59a1edaf8c209bde75c2f
AudioGod <audiogod@sonic-developers.com>
2015-08-11 07:22:08 +0000

    SOKPify
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>

--- sokp.mk
+++ sokp.mk
@@ -17,10 +17,10 @@
 $(call inherit-product, device/samsung/trltespr/full_trltespr.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/sokp/config/nfc_enhanced.mk)
 
-# Inherit common CM phone.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common SOKP phone.
+$(call inherit-product, vendor/sokp/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := trltespr
-PRODUCT_NAME := cm_trltespr
+PRODUCT_NAME := sokp_trltespr
